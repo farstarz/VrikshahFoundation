4e31843b702f046219f52b270b1b0e5948b87e8b
Farstarz <vaibhav.pandey9890@gmail.com>
2018-10-13 19:04:46 -0700

    added pseudo code

--- assets/firebase/functions/index.js
+++ assets/firebase/functions/index.js
@@ -1,5 +1,4 @@
   const functions = require('firebase-functions');
-
   // // Create and Deploy Your First Cloud Functions
   // // https://firebase.google.com/docs/functions/write-firebase-functions
   //
@@ -7,13 +6,20 @@
   //  response.send("Hello from Firebase!");
   // });
 
-  exports.welcomeEmail = functions.database.ref('welcomeEmail/{welcomeId}').onCreate((snapshot, context) => {
-      const id = context.params.welcomeId;
-      console.log("ID: " + id);
-      const original = snapshot.val();
-      // console.log("WTF: " + original);
-      var http = require("https");
 
+  exports.example = functions.database.ref('testing/{testId}').onCreate((snapshot, context) => {
+    const id = context.params.testId;
+    console.log("ID: " + id);
+    const original = snapshot.val();
+    console.log("WTF: " + original);
+    const uppercase = original.toUpperCase();
+
+    return snapshot.ref.update( { id: uppercase});
+  });
+
+  exports.welcomeEmail = functions.database.ref('welcomeEmail/{welcomeID}').onCreate((snapshot, context) => {
+    var http = require("http");
+  
       var options = {
         "method": "POST",
         "hostname": "api.pepipost.com",
@@ -38,15 +44,49 @@
         });
       });
 
-      req.write(JSON.stringify({ personalizations:  { recipient: ['vaibhav.pandey9890@gmail.com', 'margaretful@gmail.com']},
+      req.write(JSON.stringify({ personalizations:  [{recipient:'mlhe@ucdavis.edu'},{recipient:'vaibhav.pandey9890@gmail.com'}],
         from: { fromEmail: 'farstarz@pepisandbox.com', fromName: 'farstarz' },
         subject: 'Welcome to Pepipost',
-        content: 'Hi, this email is getting sent to multiple people' }));
+        content: 'Hi, this email is getting sent to multiple people TO MARGRATE and ILU THIS ONE' }));
       req.end();
-      const uppercase = original.toUpperCase;
 
-      return snapshot.ref.update( { id: uppercase});
+      
+      return 0;
   });
+
+
+  // exports.getEmails = functions.database.ref('{year}/{month}/{day}').once("value").then((snapshot)=>{
+  //   console.log(snapshot);
+      // get emails from firebase
+          // create a trigger at a certain time
+          // check today's data and store year month and date in variables
+          // in firebase check for corresponding year, corresponding month and corresponding date
+          // if snapshot exists, store the eventIDs in a array
+        // create array of emails for notification
+         // for each eventID, go to eventAttendees collection
+         // parse through all users, for every user with notification ON add userID to an array <lc>
+            // of objects with key set to recipient
+            //  for (var i =0; i<oldarray.length; i++){
+            //    newarray[i] = `{recipient:`+oldarray[i]+`}`;
+            //  }
+          // pass this array to sendEmail function
+      
+      // input this array as parameter in the sendEmail function
+
+
+
+
+
+  //   return 0;
+  // });
+
+  // get date and time
+  // var today = new Date();
+  // today -= "0000-00-00T07:00:00.000Z";
+  // var dd = today.getDate();
+  // var mm = today.getMonth()+1;
+  // var yyyy = today.getFullYear();
+  // console.log(mm+'/'+dd+'/'+yyyy);
 
 
 
